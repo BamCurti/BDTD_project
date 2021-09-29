9d28464a68952b4f20fe34901029074f79bbcfaf
Edgar Vallejo Curti <bam_vllj98@outlook.com>
2021-09-29 10:42:52 -0500

    Procedure debuged, still pending color on article

--- cubo_ventas/poblar_DIMVentas.sql
+++ cubo_ventas/poblar_DIMVentas.sql
@@ -3,6 +3,10 @@ AS
 BEGIN
 	--ELIMINAR DATA PREVIA
 	DELETE PROYECTO1_BDTD.DBO.FactSales;
+	DELETE PROYECTO1_BDTD.DBO.FactCompra;
+	DELETE PROYECTO1_BDTD.dbo.DimComprador;
+	delete PROYECTO1_BDTD.dbo.DimProveedor;
+	DELETE PROYECTO1_BDTD.DBO.DimSubCompra;
 	DELETE PROYECTO1_BDTD.DBO.DimArticulo;
 	DELETE PROYECTO1_BDTD.DBO.DimCliente;
 	DELETE PROYECTO1_BDTD.DBO.DimSubFactura;
@@ -12,7 +16,17 @@ BEGIN
 --POBLAR LA DIMENSIÓN ARTICULO !!!!FALTA COLOR!!!!!!
 INSERT INTO PROYECTO1_BDTD.dbo.DimArticulo (Id_Articulo, Descripcion, Codigo, ColorBase, ColorDerivado, UMD, Tipo, Grupo)
 
-	SELECT a.id_articulo, a.descripcion, a.codigo, null, null, u.descripcion, a_t.descripcion, a_g.descripcion
+	SELECT a.id_articulo, a.descripcion, a.codigo,
+	--color base
+		case
+		WHEN a.descripcion like('%BLANCO%') THEN 'BLANCO'
+		When a.descripcion like('%NEGRO%') THEN 'NEGRO'
+		WHEN A.descripcion like('%AZUL%') THEN 'AZUL'
+		WHEN A.descripcion LIKE('%ROJO%') THEN 'ROJO'
+		WHEN A.descripcion like('%AMARILLO%') THEN 'AMARILLO'
+		ELSE 'OTRO'
+	end,
+	null, u.descripcion, a_t.descripcion, a_g.descripcion
 	FROM PinturaO2021.DBO.Articulo a, PinturaO2021.dbo.ArticuloGrupo a_g,
 	PinturaO2021.dbo.ArticuloTipo a_t, PinturaO2021.dbo.umd u
 	WHERE A.id_articulogrupo = a_g.id_articulogrupo
@@ -90,10 +104,10 @@ insert into PROYECTO1_BDTD.dbo.FactSales(#num_fact, #num_articulos, #total, #sub
 	FROM a_x_f axf,
 	PinturaO2021.dbo.Factura f, PinturaO2021.dbo.Factura_d f_d
 	where f.Id_Factura = f_d.Id_Factura
-	and axf.id_factura = f_d.Id_Factura
+	and axf.id_factura = f_d.Id_Factura;
 
 --POBLAR LA SUB DIMENSIÓN COMPRA
-insert into PROYECTO1_BDTD.dbo.DimProveedor(id_compra,Folio,FormaPago)
+insert into PROYECTO1_BDTD.dbo.DimSubCompra(id_compra,Folio,FormaPago)
 	select pf.id_ProveedorFactura, pf.Folio, fp.Descripcion
 	from PinturaO2021.dbo.ProveedorFactura pf
 	inner join PinturaO2021.dbo.ProveedorFormaPago fp 
@@ -105,33 +119,39 @@ insert into PROYECTO1_BDTD.dbo.DimComprador(id_comprador,nombre)
 	from PinturaO2021.dbo.Comprador;
 
 --POBLAR LA SUB DIMENSIÓN PROVEEDOR
-insert into (Id_proveedor, RazonSocial, Colonia, Ciudad, Estado, Pais)
+insert into PROYECTO1_BDTD.DBO.DimProveedor(Id_proveedor, RazonSocial, Colonia, Ciudad, Estado, Pais)
 	select Id_Proveedor, RazonSocial, Colonia, c.nombre, c.estado, c.pais
 	from PinturaO2021.dbo.Proveedor p
 	inner join PinturaO2021.dbo.Ciudad c on (c.id_ciudad=p.id_Ciudad);
 
  --POBLAR LA DIMENSIÓN FACT COMPRA
+ with cant_x_fact as
+ (
+	select id_proveedorfactura, sum(Cantidad) as cantidad
+	from PinturaO2021.dbo.ProveedorFactura_d
+	group by id_proveedorfactura
+ )
 insert into PROYECTO1_BDTD.dbo.FactCompra(#cantidad_compra, #total, #subtotal,
 	#descuento,	#IVA, #cantidad_articulos, 
 	Id_tiempo, Id_articulo, Id_proveedor, Id_comprador, Id_compra)
-	SELECT f_d.cantidad, f_d.total, f_d.subtotal, f_d.Descuento, f_d.iva, 
-		1 as cantidad_articulos,
+	SELECT f_d.Cantidad / cant_x_fact.cantidad, --cantidad compra
+	f_d.total, f_d.subtotal, f_d.Descuento, f_d.iva, 
+		f_d.Cantidad,	
 		cast( --idtiempo
 			SUBSTRING(convert(varchar, f.fecha, 126),1,4) +
 			SUBSTRING(convert(varchar, f.fecha, 126),6,2) +
 			SUBSTRING(convert(varchar, f.fecha, 126),9,2)
-		as bigint) as tiempo, 
+		as bigint), 
 		f_d.id_articulo,
-		f_d.id_proveedorfactura,
+		f.id_Proveedor,
 		f.id_comprador,
-		f.id_Proveedor	
+		f.id_ProveedorFactura
 
-	from PinturaO2021.dbo.ProveedorFactura f, PinturaO2021.dbo.ProveedorFactura_d f_d
-	where f.id_ProveedorFactura = f_d.id_proveedorfactura;
+	from PinturaO2021.dbo.ProveedorFactura f, PinturaO2021.dbo.ProveedorFactura_d f_d, cant_x_fact
+	where f.id_ProveedorFactura = f_d.id_proveedorfactura
+	and cant_x_fact.id_proveedorfactura = f_d.id_proveedorfactura;
 
 END
 
 --EXECUTE POBLAR_PROYECTO1_BDTD;
---select * from PROYECTO1_BDTD.dbo.Factsales order by id_factura;
-
-	
+select * from PROYECTO1_BDTD.dbo.DimArticulo
